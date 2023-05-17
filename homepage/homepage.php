24a9a21f4bfa8c1770ce7aa5e86b5e00087911d3
kaitlinfrani <kaitlinfrani@csu.fullerton.edu>
2023-05-17 09:31:59 -0700

    homepage styling finished

--- homepage/homepage.php
+++ homepage/homepage.php
@@ -14,11 +14,13 @@
 </head>
 
 <body>
-    <h1>Welcome to MediMeet</h1>
-    <div class="button-container">
-        <a href="/project/login/login-client.php" class="button">Client Login</a>
-        <a href="/project/login/login-office.php" class="button">Medical Office Login</a>
-        <a href="/project/signup/signup.php" class="button">Signup</a>
+    <div class="container">
+        <h1>Welcome to MediMeet</h1>
+        <div class="button-container">
+            <a href="/project/login/login-client.php" class="button">Client Login</a>
+            <a href="/project/login/login-office.php" class="button">Medical Office Login</a>
+            <a href="/project/signup/signup.php" class="button">Signup</a>
+        </div>
     </div>
 </body>
 
